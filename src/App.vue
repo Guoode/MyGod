d4c42ec886dceb5f77af7a65e1b170c663688512
Guoode <319788742@qq.com>
2024-07-18 19:52:41 +0800

    4

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,27 @@
 <template>
-  <div
-    class="bg-[url('assets/imgs/bg.jpg')] bg-cover bg-center min-h-screen text-white p-2 flex flex-col flex-grow"
-  >
-    <top class="bg-opacity-50 bg-slate-800 p-3 h-1/5 w-full" />
-    <div class="flex flex-1 mt-2 flex-grow">
-      <div class="flex-1 mr-2 flex flex-col">
-        <map class="bg-opacity-50 bg-slate-800 p-3 mt-2 flex-1" />
-      </div>
-
-      <!-- 右 -->
-      <div
-        class="w-1/3 ml-5 bg-opacity-50 bg-slate-800 p-3 flex flex-col h-full"
-      >
-        <log class="h-full box-border pb-4" />
+  <div class="page-wrapper">
+    <div
+      class="bg-[url('assets/imgs/bg.jpg')] bg-cover bg-center min-h-screen text-white p-2 flex flex-col flex-grow"
+    >
+      <top class="bg-opacity-50 bg-slate-800 p-3 h-1/5 w-full" />
+      <div class="flex flex-1 mt-2 flex-grow">
+        <div class="flex-1 mr-2 flex flex-col">
+          <map class="bg-opacity-50 bg-slate-800 p-3 mt-2 flex-1" />
+        </div>
+        <div
+          class="w-1/3 ml-5 bg-opacity-50 bg-slate-800 p-3 flex flex-col h-full"
+        >
+          <log class="h-full box-border pb-4" />
+        </div>
       </div>
     </div>
+    <bottom class="mt-2" />
   </div>
 </template>
 
 <script setup>
 import top from "./components/top.vue"; //顶部
-import map from "./components/map.vue";
+import map from "./components/map.vue"; //地图
 import log from "./components/log.vue"; //日志
-//import bottom from "./components/bottom.vue";//底部
+import bottom from "./components/bottom.vue"; //底部
 </script>
---
+++ src/components/bottom.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>底部</div>
+</template>
+<script>
+export default {
+  name: "bottom",
+  data() {
+    return {
+      msg: "底部",
+    };
+  },
+};
+</script>
