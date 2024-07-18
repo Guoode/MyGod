ecbeb27bba82b1cbbd8aa014a926d4f03d4899fe
Guoode <319788742@qq.com>
2024-07-18 20:05:26 +0800

    4

--- src/components/bottom.vue
+++ src/components/bottom.vue
@@ -1,13 +1,30 @@
 <template>
-  <div>底部</div>
+  <footer class="bg-gray-800 text-white">
+    <div class="bg-gray-900 py-5 text-center">
+      <div class="container mx-auto">
+        <p class="text-sm text-gray-400">
+          <a href="javascript:;" class="mr-2">关于我们</a>
+          <a href="javascript:;" class="mr-2">帮助中心</a>
+          <a href="javascript:;" class="mr-2">商务合作</a>
+          <a href="javascript;">友情链接</a>
+        </p>
+        <p class="text-xs text-gray-400 mt-2">
+          Copyright © 2022-2024 永远的神 版权所有
+        </p>
+      </div>
+    </div>
+  </footer>
 </template>
+
 <script>
 export default {
-  name: "bottom",
-  data() {
-    return {
-      msg: "底部",
-    };
-  },
+  name: "AppFooter",
 };
 </script>
+
+<style scoped>
+.container {
+  max-width: 1200px;
+  margin: 0 auto;
+}
+</style>
