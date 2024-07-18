a9cd40c5e844c2f0f17d50c4413324a0feaa4cdb
Guoode <319788742@qq.com>
2024-07-18 20:53:00 +0800

    2053

--- src/components/maps.vue
+++ src/components/maps.vue
@@ -1,13 +1,92 @@
 <template>
-  <div>地图</div>
+  <div class="map-container">
+    <baidu-map
+      class="map"
+      :center="{ lng: centerLng, lat: centerLat }"
+      :zoom="10"
+      :scroll-wheel-zoom="true"
+      @ready="mapReady"
+    >
+      <bm-scale anchor="BMAP_ANCHOR_TOP_RIGHT"></bm-scale>
+      <bm-geolocation
+        anchor="BMAP_ANCHOR_BOTTOM_RIGHT"
+        :showAddressBar="true"
+        @locationSuccess="locationSuccess"
+      ></bm-geolocation>
+      <bm-marker
+        v-for="device in devices"
+        :key="device.id"
+        :position="{ lng: device.lng, lat: device.lat }"
+        @click="selectDevice(device)"
+      >
+        <bm-label
+          :content="device.name"
+          :labelStyle="{ color: 'red', fontSize: '12px' }"
+          :offset="{ width: 20, height: -10 }"
+        />
+      </bm-marker>
+    </baidu-map>
+  </div>
 </template>
+
 <script>
 export default {
-  name: "maps",
   data() {
     return {
-      msg: "地图",
+      centerLng: 114.3, // 湖北省中心经度
+      centerLat: 30.6, // 湖北省中心纬度
+      devices: [],
     };
+  },
+  mounted() {
+    this.generateDevices(50);
+  },
+  methods: {
+    mapReady({ BMap, map }) {
+      this.map = map;
+    },
+    locationSuccess(e) {
+      this.centerLng = e.point.lng;
+      this.centerLat = e.point.lat;
+    },
+    generateDevices(count) {
+      const bounds = this.getBoundsHubei();
+      const statuses = ["正常", "警告", "离线"];
+      for (let i = 0; i < count; i++) {
+        this.devices.push({
+          id: i + 1,
+          name: `设备${i + 1}`,
+          lng: Math.random() * (bounds.maxX - bounds.minX) + bounds.minX,
+          lat: Math.random() * (bounds.maxY - bounds.minY) + bounds.minY,
+          status: statuses[Math.floor(Math.random() * statuses.length)],
+          battery: Math.floor(Math.random() * 100),
+        });
+      }
+    },
+    getBoundsHubei() {
+      return {
+        minX: 108.9,
+        maxX: 116.05,
+        minY: 29.3,
+        maxY: 33.15,
+      };
+    },
+    selectDevice(device) {
+      // 触发一个自定义事件，将选中的设备信息传递给父组件
+      this.$emit("device-selected", device);
+    },
   },
 };
 </script>
+
+<style>
+.map-container {
+  width: 100%;
+  height: 500px;
+}
+
+.map {
+  width: 100%;
+  height: 100%;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,11 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
+import BaiduMap from 'vue-baidu-map-3x'
 
-createApp(App).mount('#app')
+const app = createApp(App);
+app.use(BaiduMap, {
+  ak: 'ERJaT2zREz57EesvlgMEX0fp7aMP1p8k',
+});
+// 挂载应用实例到 DOM
+app.mount('#app');
