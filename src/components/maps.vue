bdbca55f3baa513fd989a8f1a1874ec4edb8d87a
Guoode <319788742@qq.com>
2024-07-19 16:39:32 +0800

    1

--- src/components/maps.vue
+++ src/components/maps.vue
@@ -17,11 +17,14 @@
         v-for="device in devices"
         :key="device.id"
         :position="{ lng: device.lng, lat: device.lat }"
-        @click="selectDevice(device)"
+        @click="openInfoWindow(device)"
       >
         <bm-label
           :content="device.name"
-          :labelStyle="{ color: 'red', fontSize: '12px' }"
+          :labelStyle="{
+            color: device.status === '正常' ? 'green' : 'red',
+            fontSize: '12px',
+          }"
           :offset="{ width: 20, height: -10 }"
         />
       </bm-marker>
@@ -60,6 +63,14 @@ export default {
           lat: Math.random() * (bounds.maxY - bounds.minY) + bounds.minY,
           status: statuses[Math.floor(Math.random() * statuses.length)],
           battery: Math.floor(Math.random() * 100),
+          meid: `MEID${i + 1}`, // 添加 MEID 属性
+          mobile: `1380${i + 1}`, // 添加手机号属性
+          osVersion: `v${Math.floor(Math.random() * 5)}.${Math.floor(
+            Math.random() * 10
+          )}`, // 添加版本属性
+          perCpu: `${Math.floor(Math.random() * 100)}%`, // 添加 CPU 性能属性
+          perRam: `${Math.floor(Math.random() * 100)}%`, // 添加 RAM 性能属性
+          perDisk: `${Math.floor(Math.random() * 100)}%`, // 添加 DISK 性能属性
         });
       }
     },
@@ -71,9 +82,23 @@ export default {
         maxY: 33.15,
       };
     },
-    selectDevice(device) {
-      // 触发一个自定义事件，将选中的设备信息传递给父组件
-      this.$emit("device-selected", device);
+    openInfoWindow(device) {
+      const opts = {
+        width: 300, // 信息窗口宽度
+        height: 150, // 信息窗口高度
+        title: "设备信息", // 信息窗口标题
+        message: "",
+      };
+      const infoWindowContent = `
+        <span style='display:inline-block;width:60px; text-align: right;font-weight:700'>MEID：</span>${device.meid}<br/>
+        <span style='display:inline-block;width:60px; text-align: right;font-weight:700'>设备编号：</span>${device.mobile}<br/>
+        <span style='display:inline-block;width:60px; text-align: right;font-weight:700'>状态：</span>${device.status}<br/>
+        <span style='display:inline-block;width:60px; text-align: right;font-weight:700'>版本：</span>${device.osVersion}<br/>
+        <span style='display:inline-block;width:60px; text-align: right;font-weight:700'>性能：</span>CPU: ${device.perCpu} RAM: ${device.perRam} DISK: ${device.perDisk}
+      `;
+      const point = new BMap.Point(device.lng, device.lat);
+      const infoWindow = new BMap.InfoWindow(infoWindowContent, opts);
+      this.map.openInfoWindow(infoWindow, point);
     },
   },
 };
